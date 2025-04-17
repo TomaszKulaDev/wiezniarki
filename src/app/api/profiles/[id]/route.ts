1f14cafe1559b2d237989b6890f90eed3383f91b
TomaszKulaDev <tomaszkulewicz@gmail.com>
2025-04-17 23:12:34 +0200

    05

--- src/app/api/profiles/[id]/route.ts
+++ src/app/api/profiles/[id]/route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-nocheck
 import { NextRequest, NextResponse } from "next/server";
 import { profileService } from "@/backend/services/profileService";
